b06fcc2053e5d973f41b6076e16a395e0ecbf0ed
m0renets <serhii.morenets@gmail.com>
2025-05-16 15:59:10 +0300

    added tasks generator

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <chrono>
+#include <vector>
 #include <queue>
 #include <mutex>
 #include <shared_mutex>
@@ -17,6 +18,9 @@
 #define EXECUTION_INTERVAL 30
 #define MIN_TASK_TIME 2
 #define MAX_TASK_TIME 8
+#define GENERATOR_THREAD_COUNT 2
+#define GENERATOR_INTERVAL_MIN 2
+#define GENERATOR_INTERVAL_MAX 8
 
 using namespace std;
 using namespace chrono;
@@ -26,13 +30,18 @@ using write_lock = unique_lock<shared_mutex>;
 
 atomic<size_t> prev_id = 0;
 
+shared_mutex sync_mtx;
+static mutex cout_mutex;
+
 class task
 {
 
 public:
-    task() : task_id(prev_id++), task_time(random_time()) {};
+    task() : task_time(random_time()) {};
     task(size_t id, seconds time) : task_id(id), task_time(time) {}
     ~task() = default;
+
+    size_t task_id;
 
     size_t get_id() const { return task_id; }
     seconds get_time() const { return task_time; }
@@ -40,10 +49,14 @@ public:
     void execute()
     {
         this_thread::sleep_for(task_time);
+
+        {
+            lock_guard lock(cout_mutex);    
+            cout << get_id() << " task is executed time: " << get_time().count() << endl;
+        }        
     }
 
 private:
-    size_t task_id;
     seconds task_time;
 
     seconds random_time()
@@ -171,6 +184,7 @@ public:
             }
 
             task.execute();
+            //cout << task.get_id() << " task is executed< time: " << task.get_time().count() << endl;
         }
     }
 
@@ -219,7 +233,38 @@ public:
         workers.clear();
         terminated = false;
         initialized = false;
-    }    
+    }
+    
+    void force_terminate()
+    {
+
+        {
+            write_lock _(rw_lock);
+    
+            if (!working_unsafe())
+            {
+                return;
+            }
+    
+            terminated = true;
+        }
+     
+        task_waiter.notify_all();
+
+        for (thread &worker : workers)
+        {
+            if (worker.joinable())
+            {
+                worker.join();
+            }
+        }
+
+        workers.clear();
+        terminated = false;
+        initialized = false;
+    }
+
+
 
 private:
 
@@ -234,8 +279,131 @@ private:
     mutable condition_variable_any task_waiter;
 };
 
+class task_generator
+{
+    public:
+    task_generator() = default;
+    ~task_generator() {stop();};
+
+    void initialize(thread_pool & tp)
+    {
+
+        lock_guard lock(gen_mtx);
+        if (isWorking)
+        {
+            return;
+        }
+
+        isWorking = true;
+
+        generators.reserve(GENERATOR_THREAD_COUNT);
+        for (size_t id = 0; id < GENERATOR_THREAD_COUNT; id++)
+        {
+            generators.emplace_back(generate, this, ref(tp));
+        }
+    }
+
+    void stop()
+    {
+
+        lock_guard _(gen_mtx);
+
+        if (!isWorking)
+        {
+            return;
+        }
+
+        {
+            
+            isWorking = false;
+        }
+
+        for (thread &generator : generators)
+        {
+            if (generator.joinable())
+            {
+                generator.detach();
+            }
+        }
+
+        generators.clear();
+    }
+
+    private:
+
+    void generate(thread_pool & tp)
+    {
+
+        static thread_local mt19937 generator(random_device{}());
+        uniform_int_distribution<int> distribution(GENERATOR_INTERVAL_MIN, GENERATOR_INTERVAL_MAX);
+
+        while (true)
+        {
+
+            {
+                lock_guard _(gen_mtx);
+                if (!isWorking)
+                {
+                    break;
+                }
+            }
+
+            task task;
+
+            {
+                lock_guard _(gen_mtx);
+                task.task_id = prev_id++;
+                tp.add_task(move(task));   
+            }
+
+            {
+                lock_guard lock(cout_mutex);
+                cout << "Add task with id: " << task.get_id() << ", time: " << task.get_time().count() << endl;
+            }
+
+            seconds waitTime = seconds(distribution(generator));
+
+            this_thread::sleep_for(seconds(1));
+        }
+    }
+
+    vector<thread> generators;
+    bool isWorking = false;
+    mutable shared_mutex gen_mtx;
+};
+
+void test_force_terminate()
+{
+    cout << "Starting test for force_terminate()" << endl;
+
+    thread_pool tp;
+    tp.initialize(THREAD_COUNT);
+
+    task_generator tg;
+    tg.initialize(tp);
+
+    this_thread::sleep_for(seconds(5));
+    tg.stop();
+
+    this_thread::sleep_for(seconds(5));
+    tg.initialize(tp);
+
+    cin.get();
+
+}
+
+void print_prevv_id()
+{
+    while (true) {
+        cout << prev_id << endl;
+        this_thread::sleep_for(seconds(1));
+    }
+}
+
 int main()
 {
+
+    test_force_terminate();
 
     return 0;
 }
